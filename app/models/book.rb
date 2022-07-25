b4225d5429ed149f562dbc943174dc6da23ae2d5
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-07-25 07:17:23 +0000

    [Update]応用課題５

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
@@ -11,4 +11,20 @@ class Book < ApplicationRecord
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match" # 完全一致
+      @book = Book.where("title LIKE?", "#{word}")
+    elsif search == "forward_match" # 前方一致
+      @book = Book.where("title LIKE?", "#{word}%")
+    elsif search == "backward_match" # 後方一致
+      @book = Book.where("title LIKE?", "%#{word}")
+    elsif search == "partial_match" # 部分一致
+      @book = Book.where("title LIKE?", "%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  #フォローをした、されたの関係
+  # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  #一覧画面で使う
+  # 一覧画面で使う
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
@@ -23,23 +23,38 @@ class User < ApplicationRecord
   validates :introduction, length: {maximum: 50}
 
 
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
-  #フォローしたときの処理
+  # フォローしたときの処理
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
 
-  #フォローを外すときの処理
+  # フォローを外すときの処理
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
 
-  #フォローしているか判定
+  # フォローしているか判定
   def following?(user)
     followings.include?(user)
+  end
+
+
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match" # 完全一致
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match" # 前方一致
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match" # 後方一致
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match" # 部分一致
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
   end
 end
